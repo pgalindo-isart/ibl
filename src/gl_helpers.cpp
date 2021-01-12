b856b716f259192abbbd452de852ec0e8b62148f
Paul Galindo <p.galindo@isartdigital.com>
2021-01-12 16:20:13 +0100

    Add DDS (cubemap-only) parsing

--- src/gl_helpers.cpp
+++ src/gl_helpers.cpp
@@ -1,11 +1,13 @@
 
 #include <cstdio>
+#include <fstream>
 #include <vector>
 
 #include <stb_perlin.h>
 #include <stb_image.h>
 
 #include "types.hpp"
+#include "calc.hpp"
 #include "gl_helpers.hpp"
 
 GLuint gl::CreateShader(GLenum type, int sourceCount, const char** sources)
@@ -137,4 +139,96 @@ void gl::SetTextureDefaultParams(bool genMipmap)
 
     if (GLAD_GL_EXT_texture_filter_anisotropic)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.f);
+}
+
+
+#define DDS_SURFACE_FLAGS_CUBEMAP 0x00000008 // DDSCAPS_COMPLEX
+
+#define DDS_CUBEMAP_POSITIVEX 0x00000600 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_POSITIVEX
+#define DDS_CUBEMAP_NEGATIVEX 0x00000a00 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_NEGATIVEX
+#define DDS_CUBEMAP_POSITIVEY 0x00001200 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_POSITIVEY
+#define DDS_CUBEMAP_NEGATIVEY 0x00002200 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_NEGATIVEY
+#define DDS_CUBEMAP_POSITIVEZ 0x00004200 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_POSITIVEZ
+#define DDS_CUBEMAP_NEGATIVEZ 0x00008200 // DDSCAPS2_CUBEMAP | DDSCAPS2_CUBEMAP_NEGATIVEZ
+
+#define DDS_CUBEMAP_ALLFACES ( DDS_CUBEMAP_POSITIVEX | DDS_CUBEMAP_NEGATIVEX |\
+                               DDS_CUBEMAP_POSITIVEY | DDS_CUBEMAP_NEGATIVEY |\
+                               DDS_CUBEMAP_POSITIVEZ | DDS_CUBEMAP_NEGATIVEZ )
+
+struct DDSPixelFormat
+{
+    uint32_t size;
+    uint32_t flags;
+    uint32_t fourCC;
+    uint32_t rgbBitCount;
+    uint32_t rBitMask;
+    uint32_t gBitMask;
+    uint32_t bBitMask;
+    uint32_t aBitMask;
+};
+
+struct DDSHeader
+{
+    uint32_t       size;
+    uint32_t       flags;
+    uint32_t       height;
+    uint32_t       width;
+    uint32_t       pitchOrLinearSize;
+    uint32_t       depth;
+    uint32_t       mipMapCount;
+    uint32_t       reserved1[11];
+    DDSPixelFormat pixelFormat;
+    uint32_t       caps;
+    uint32_t       caps2;
+    uint32_t       caps3;
+    uint32_t       caps4;
+    uint32_t       reserved2;
+};
+
+void gl::UploadCubemap(const char* filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+
+    // Parse magic number
+    char magic[4] = {};
+    file.read((char*)&magic, sizeof(uint32_t));
+    if (strncmp(magic, "DDS ", 4) != 0)
+    {
+        fprintf(stderr, "Not a dds file\n");
+        return;
+    }
+
+    // Parse header
+    DDSHeader header = {};
+    file.read((char*)&header, sizeof(DDSHeader));
+
+    // TODO: Check this
+    //file.seekg(std::ios_base::cur, header.size - sizeof(DDSHeader));
+
+    // TODO: Abort loading if not cubemap...
+    if ((header.caps  & DDS_SURFACE_FLAGS_CUBEMAP) == 0 || 
+        (header.caps2 & DDS_CUBEMAP_ALLFACES)      == 0)
+    {
+        fprintf(stderr, "Not a cubemap or not complete\n");
+    }
+
+    printf("dimensions: %dx%d\n", header.width, header.height);
+
+    // Compute remaining file size
+    int imageSize = header.width * header.height;
+    std::vector<float4> data(imageSize);
+
+    for (int i = 0; i < 6; ++i)
+    {
+        for (uint32_t level = 0; level <= header.mipMapCount; ++level)
+        {
+            int width  = (int)(header.width  / calc::Pow(2, (float)level));
+            int height = (int)(header.height / calc::Pow(2, (float)level));
+            int size   = width * height;
+
+            file.read((char*)data.data(), size * sizeof(float4));
+
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, level, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, data.data());
+        }
+    }
 }
--- src/gl_helpers.hpp
+++ src/gl_helpers.hpp
@@ -10,5 +10,6 @@ namespace gl
     GLuint CreateProgram(int vsStrsCount, const char** vsStrs, int fsStrsCount, const char** fsStrs);
     void UploadPerlinNoise(int width, int height, float z, float lacunarity = 2.f, float gain = 0.5f, float offset = 1.f, int octaves = 6);
     void UploadImage(const char* file, bool linear = false);
+    void UploadCubemap(const char* filename);
     void SetTextureDefaultParams(bool genMipmap = true);
 }
